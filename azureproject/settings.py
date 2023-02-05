c5f120d3cbf8e3c1b1dbe6f19145187997b86b11
Varunv20 <varun.viges@icloud.com>
2023-02-05 12:23:02 -0600

    Revert "rename"
    This reverts commit 67c11f007353994d3eeb1676e51303c245e48dfb.

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -34,7 +34,7 @@ if 'CODESPACE_NAME' in os.environ:
 # Application definition
 
 INSTALLED_APPS = [
-    'Financial_Website.apps.RestaurantReviewConfig',
+    'restaurant_review.apps.RestaurantReviewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
