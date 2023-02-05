67c11f007353994d3eeb1676e51303c245e48dfb
Varunv20 <varun.viges@icloud.com>
2023-02-05 12:03:25 -0600

    rename

--- Financial_Website/views.py
+++ Financial_Website/views.py
@@ -5,7 +5,7 @@ from django.db.models import Avg, Count
 from django.urls import reverse
 from django.utils import timezone
 
-from restaurant_review.models import Restaurant, Review
+from Financial_Website.models import Restaurant, Review
 
 # Create your views here.
 
