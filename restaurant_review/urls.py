948472105c18c35aecb871dfde7a4c5f9c1089f3
Varunv20 <108890729+Varunv20@users.noreply.github.com>
2023-02-15 10:40:57 -0600

    Update urls.py

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index'),
     path('sign_in', views.create_signin_page, name='sign_in_page'),
