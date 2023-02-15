5a92be9820d3180bf6d2ab0497f5f81f6f0b6a69
Varunv20 <108890729+Varunv20@users.noreply.github.com>
2023-02-15 10:39:46 -0600

    Update urls.py

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('profile', views.sign_in, name='sign_in'),
     path('dashboard/', views.create_account, name='create_account'),
     path('', views.logout_view, name='logout'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'), 
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password_reset_complete.html'), name='password_reset_complete'), 
     path("password_reset", views.password_reset_request, name="password_reset")
 
     
