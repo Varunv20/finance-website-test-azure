7bf704ce32a73ee944e475d759db86e13e19a101
Varunv20 <108890729+Varunv20@users.noreply.github.com>
2023-02-15 08:39:20 -0600

    Update urls.py

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -8,11 +8,15 @@ urlpatterns = [
     path('create_account', views.create_account_page, name='create_account_page'),
 
 
-    path('profile', views.create_profile, name='user_profile'),
+    path('profile', views.sign_in, name='sign_in'),
     path('dashboard/', views.create_account, name='create_account'),
+    path('', views.logout_view, name='logout'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'), 
+    path("password_reset", views.password_reset_request, name="password_reset")
 
-]
+    
+    
 
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
